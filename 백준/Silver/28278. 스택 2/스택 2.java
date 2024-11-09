dc6545d904f1448e49bf92b47dd634c9a8a634d9
Min Gyeong Kim <113525126+mk-star@users.noreply.github.com>
2024-11-09 23:23:35 +0900

    [Silver IV] Title: 스택 2, Time: 720 ms, Memory: 181868 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,64 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Stack {
-    int capacity;
-    int top;
-    int[] arr;
-
-    Stack(int size) {
-        arr = new int[size];
-        capacity = size;
-        top = -1;
-    }
-
-    public void push(int data) {
-        arr[++top] = data;
-    }
-    public int isEmpty() {
-        if(top == -1) {
-            return 1;
-        }
-        return 0;
-    }
-    public int pop() {
-        if(isEmpty() == 1) {
-            return -1;
-        }
-        return arr[top--];
-    }
-    public int peek() {
-        if (isEmpty() == 1) {
-            return -1;
-        }
-        return arr[top];
-    }
-    public int size() {
-        return top + 1;
-    }
-}
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        Stack stack = new Stack(n);
+        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
         for(int i = 0; i < n; i++) {
-            StringTokenizer st  = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
+            String str = br.readLine();
+            int num = Integer.parseInt(str.substring(0, 1));
             if(num == 1) {
-                stack.push(Integer.parseInt(st.nextToken()));
+                stack.push(Integer.parseInt(str.substring(2)));
             } else if(num == 2) {
-                sb.append(stack.pop()).append("\n");
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
             } else if(num == 3) {
                 sb.append(stack.size()).append("\n");
             } else if(num == 4) {
-                sb.append(stack.isEmpty()).append("\n");
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
             } else if(num == 5) {
-                sb.append(stack.peek()).append("\n");
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
             }
         }
         System.out.println(sb.toString());
