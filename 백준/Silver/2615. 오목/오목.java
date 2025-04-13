9b48a41920d11551219e1b522d5648418a6191d7
MinGyeong Kim <ssunn0812@gmail.com>
2025-04-13 13:21:56 +0900

    [Silver I] Title: 오목, Time: 124 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -3,137 +3,63 @@ import java.util.*;
 
 public class Main {
     static int[][] board;
+    // 순서대로 오른쪽, 아래, 위대각선, 아래대각선
+    static int[] dx = {0, 1, -1, 1};
+    static int[] dy = {1, 0, 1, 1};
 
-   static boolean solution(int i, int j) {
-        // 오른쪽
-        int x = i;
-        int y = j + 1;
-        int cnt = 1;
-        while(y < 19) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
+    static boolean solution(int i, int j) {
+        int[] cnt = {1, 1, 1, 1};
+        
+        for(int t = 0; t < 4; t++) {
+            int x = i + dx[t];
+            int y = j + dy[t]; 
+
+            while(true) {
+                if(x >= 0 && x < 19 && y >= 0 && y < 19) {
+                    if(board[i][j] == board[x][y]) {
+                        cnt[t]++;
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                x += dx[t];
+                y += dy[t]; 
             }
-            y += 1;
         }
 
-        // 왼쪽
-        x = i;
-        y = j - 1;
-        while(y >= 0) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
+        for(int t = 0; t < 4; t++) {
+            int x = i - dx[t];
+            int y = j - dy[t]; 
+
+            while(true) {
+                if(x >= 0 && x < 19 && y >= 0 && y < 19) {
+                    if(board[i][j] == board[x][y]) {
+                        cnt[t]++;
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                x -= dx[t];
+                y -= dy[t]; 
             }
-            y -= 1;
-        }
 
-        if(cnt == 5) {
-            System.out.println(board[i][j]);
-            System.out.println((x + 1) + " " + (y + 2));
-            return true;
-        }
-
-        // 아래
-        x = i + 1;
-        y = j;
-        cnt = 1;
-        while(x < 19) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
+            if(cnt[t] == 5) {
+                if(t == 2) {
+                    System.out.println(board[i][j]);
+                    System.out.println(x + " " + (y + 2));
+                } else {
+                    System.out.println(board[i][j]);
+                    System.out.println((i + 1) + " " + (j + 1));
+                }
+                return true;
             }
-            x += 1;
-        }
-
-        // 위
-        x = i - 1;
-        y = j;
-        while(x >= 0) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
-            }
-            x -= 1;
-        }
-
-        if(cnt == 5) {
-            System.out.println(board[i][j]);
-            System.out.println((x + 2) + " " + (y + 1));
-            return true;
-        }
-
-        // 위 대각선(위)
-        x = i - 1;
-        y = j + 1;
-        cnt = 1;
-        while(x >= 0 && y < 19) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
-            }
-            x -= 1;
-            y += 1;
-        }
-
-        // 위 대각선(아래)
-        x = i + 1;
-        y = j - 1;
-        while(x < 19 && y >= 0) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
-            }
-            x += 1;
-            y -= 1;
-        }
-
-        if(cnt == 5) {
-            System.out.println(board[i][j]);
-            System.out.println(x + " " + (y + 2));
-            return true;
-        }
-
-        // 아래 대각선(아래)
-        x = i + 1;
-        y = j + 1;
-        cnt = 1;
-        while(x < 19 && y < 19) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
-            }
-            x += 1;
-            y += 1;
-        }
-
-        // 아래 대각선(위)
-        x = i - 1;
-        y = j - 1;
-        while(x >= 0 && y >= 0) {
-            if(board[x][y] == board[i][j]) {
-                cnt++;
-            } else {
-                break;
-            }
-            x -= 1;
-            y -= 1;
-        }
-
-        if(cnt == 5) {
-            System.out.println(board[i][j]);
-            System.out.println((x + 2) + " " + (y + 2));
-            return true;
         }
         return false;
-   }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -150,7 +76,7 @@ public class Main {
             for(int j = 0; j < 19; j++) {
                 if(board[i][j] != 0) {
                     if (solution(i, j)) {
-                        System.exit(0);
+                        return;
                     }
                 }
             }
