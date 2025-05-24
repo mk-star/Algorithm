915c147f842f29687c6c40dffce0087103399329
MinGyeong Kim <ssunn0812@gmail.com>
2025-05-24 11:08:45 +0900

    [level 2] Title: 스킬트리, Time: 0.45 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,30 +1,20 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = -1;
+        int answer = 0;
         
-        for(int t = 0; t < skill_trees.length; t++) {
-            int len = skill.length();
-            String s = skill_trees[t]; 
-            int[] num = new int[len + 1]; 
-            for(int i = 0; i < len; i++) { 
-                num[i] = s.length();
-                for(int j = 0; j < s.length(); j++) {
-                    if(skill.charAt(i) == s.charAt(j)) {
-                        num[i] = j;
-                        break;
-                    }
-                }
-            }
+        for (int i = 0; i < skill_trees.length; i++) {
+			String[] s = skill_trees[i].split("");
+			int index = 0;
             
-            for(int i = 1; i < len; i++) {
-                if(num[i] < num[i - 1]) break;
-                if(i == len - 1) {
-                    answer = answer == -1 ? 1 : answer + 1;
-                }
-            }
-            if(len == 1) answer = skill_trees.length;
-        }
-        if(answer == -1) return 0;
-        else return answer;
+			for (int j = 0; j < s.length; j++) {
+				if (index < skill.indexOf(s[j])) {
+					break;
+				} else if (index == skill.indexOf(s[j])) {
+					index++;
+				}
+                if (j == s.length - 1) answer++;
+			}
+		}
+		return answer;
     }
 }
