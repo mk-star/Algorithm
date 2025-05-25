fb47258de702493e644b5d926e12cd8ec4f137d6
MinGyeong Kim <ssunn0812@gmail.com>
2025-05-25 15:55:58 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,30 +2,23 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
-        int count = n;
+        int count = 0;
         for(int i = 0; i < n; i++) {
-            int tempC = bfs(i, n, computers, visited);
-            if(tempC != 0) count -= tempC;
+            if(!visited[i]) {
+                dfs(computers, visited, i);
+                count++;
+            }
         }
         return count;
     }
     
-    public int bfs(int c, int n, int[][] computers, boolean[] visited) {
-        Queue<Integer> queue = new LinkedList<>();
-        int count = 0;
+    public void dfs(int[][] computers, boolean[] visited, int start) {
+        visited[start] = true;
         
-        queue.offer(c);
-        visited[c] = true;
-        while(!queue.isEmpty()) {
-            int v = queue.poll();
-            for(int i = 0; i < n; i++) {
-                if(v != i && computers[v][i] == 1 && !visited[i]) {
-                    queue.offer(i);
-                    visited[i] = true;
-                    count++;
-                }
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[start][i] == 1 && !visited[i]) {
+                dfs(computers, visited, i);
             }
         }
-        return count;
     }
 }
