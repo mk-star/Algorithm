a79243aec48e744958c3d1ecbbcf46d9c37a59ca
MinGyeong Kim <ssunn0812@gmail.com>
2025-06-20 22:55:58 +0900

    [level 1] Title: 2016년, Time: 4.80 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,5 +1,5 @@
 function solution(a, b) {
     const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    const date = 2016 + '-' + a + '-' + b
+    const date = `2016-${a}-${b}`;
     return dayOfWeek[new Date(date).getDay()]; 
 }
