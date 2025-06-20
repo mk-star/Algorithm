3fad401c167a1245cf06036d3f6facee39f841e0
MinGyeong Kim <ssunn0812@gmail.com>
2025-06-20 22:54:23 +0900

    [level 1] Title: 과일 장수, Time: 238.70 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,16 @@
+// 3 3 2 2 1 1 1
+function solution(k, m, score) {
+    let result = 0;
+    score.sort((a, b) => b - a);
+    
+    let i = m - 1;
+    while(i < score.length) {
+        result += score[i] * m;
+        i += m;
+    }
+    return result;
+}
+// 점수: 1 ~ k
+// m개를 뽑음 -> 내림차순 정렬해서 앞에서부터 m개 뽑음
+// 가격: p(뽑은 m개 중 가장 마지막 요소)*m, 
+// 이익: 가격의 총합
