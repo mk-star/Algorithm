35d825be6364fedc6e822c79b9909d2f0ab99590
MinGyeong Kim <ssunn0812@gmail.com>
2025-06-21 11:40:13 +0900

    [level 1] Title: 실패율, Time: 5144.96 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,20 +1,13 @@
 function solution(N, stages) {
-    let players = stages.length;
-    let map = new Map();
+    let result = [];
     
     for(let i = 1; i <= N; i++) {
-        let cnt = 0;
-        for(let j = 0; j < stages.length; j++) {
-            if(stages[j] === i) {
-                cnt++;
-            }
-        }
-        map.set(i, cnt / players);
-        players -= cnt;
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        
+        result.push([i, curr / reach]); // 배열을 push -> 2차원 배열
     }
-
-    let keys = [...map.entries()]
-        .sort((a, b) => b[1] - a[1])
-        .map(([key]) => key);
-    return keys;
+    
+    result.sort((a, b) => b[1] - a[1]); // 두 번째 원소를 기준으로 정렬
+    return result.map((x) => x[0]);
 }
