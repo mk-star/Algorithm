37d380d8244bee2a7d636d845b10bec7d8c30f6f
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-03 10:27:58 +0900

    [Silver V] Title: 단어 정렬, Time: 296 ms, Memory: 25608 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,40 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static ArrayList<String> word;
-
-	static void sortWord(String s) {
-		int idx = word.size();
-
-		for(int i = 0; i < word.size(); i++) {
-			if(s.length() < word.get(i).length()) {
-				idx = i;
-				break;
-			}
-
-			if(s.length() == word.get(i).length()) {
-				if(s.compareTo(word.get(i)) < 0) {
-					idx = i;
-					break;
-				}
-			}	
-		}
-		word.add(idx, s);
-	}
 	public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-		word = new ArrayList<>();
-
+		String[] word = new String[N];
+		
 		for(int i = 0; i < N; i++) {
-			String s = br.readLine();
-			if(word.contains(s)) continue;
-			sortWord(s);
+			word[i] = br.readLine();
 		}
 
-		for(int i = 0; i < word.size(); i++) {
-			System.out.println(word.get(i));
+		Arrays.sort(word, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				if(s1.length() == s2.length()) {
+					return s1.compareTo(s2); // 사전 순으로 비교
+				} else {
+					return s1.length() - s2.length(); // 
+				}
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(word[0]).append('\n');
+
+		for (int i = 1; i < N; i++) {
+			if (!word[i].equals(word[i - 1])) {
+				sb.append(word[i]).append('\n');
+			}
 		}
+		System.out.println(sb);
     }
 }
