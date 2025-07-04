6657734ff4eee636a00492c3391c004374f31396
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-04 09:36:45 +0900

    [Gold V] Title: 암호 만들기, Time: 152 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int L;
+	static int C;
+	static String[] arr;
+
+	static boolean isValidPassword(String password) {
+		String vowel = "aeiou";
+		int vowelCount = 0;
+		int consonantCount = 0;
+
+		for(String s : password.split("")) {
+			if(vowel.contains(s)) {
+				vowelCount++;
+			} else {
+				consonantCount++;
+			}
+		}
+
+		return vowelCount >= 1 && consonantCount >= 2;
+	}
+
+	static void solution(int n, int count, String password) {
+		if(count == L) {
+			if(isValidPassword(password)) {
+				System.out.println(password);
+			}
+			return;
+		}
+
+		if(n >= C) return;
+		
+		for(int i = n; i < C; i++) {
+			solution(i + 1, count + 1, password + arr[i]);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		arr = new String[C];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < C; i++) {
+			arr[i] = st.nextToken();
+		}
+		
+		Arrays.sort(arr);
+
+		solution(0, 0, "");
+    }
+}
