04ddcf188f4635d746c4fdb5be616f58a8cb1e04
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-09 13:01:13 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1756 ms, Memory: 73776 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -14,15 +14,15 @@ public class Main {
 		visited[x][y] = true;
 
 		while(!q.isEmpty()) {
-			int[] loc = q.poll();
+			int[] tmp = q.poll();
 			for(int i = 0; i < 4; i++) {
-				int xi = dx[i] + loc[0];
-				int yi = dy[i] + loc[1];
+				int xi = dx[i] + tmp[0];
+				int yi = dy[i] + tmp[1];
 				
 				if(xi >= 0 && yi >= 0 && xi < n && yi < m) {
 					if(!visited[xi][yi] && map[xi][yi] == 1) {
 						visited[xi][yi] = true;
-						result[xi][yi] = result[loc[0]][loc[1]] + 1;
+						result[xi][yi] = result[tmp[0]][tmp[1]] + 1;
 						q.offer(new int[]{xi, yi});
 					}
 				}
@@ -46,22 +46,17 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < m; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 0) result[i][j] = 0;
-				else result[i][j] = -1;
-			}
-		}
-
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				if(map[i][j] == 2) {
+				if(map[i][j] == 0) {
+					result[i][j] = 0;
+				} else if(map[i][j] == 2) {
 					tx = i; ty = j;
-					break;
+					result[i][j] = 0;
+				} else {
+					result[i][j] = -1;
 				}
 			}
 		}
 
-		result[tx][ty] = 0;
 		bfs(tx, ty);
 
 		for(int i = 0; i < n; i++) {
