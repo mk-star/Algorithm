a6b9435c2513eb6a8f01edadbc13a4e9ed5d72a9
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-09 11:00:10 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1844 ms, Memory: 77556 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m;
+	static int[][] map, result;
+	static boolean[][] visited; 
+	static int[] dx = {-1, 1, 0, 0}; 
+	static int[] dy = {0, 0, -1, 1};
+
+	static void bfs(int x, int y) {
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[]{x, y, 0});
+		visited[x][y] = true;
+
+		while(!q.isEmpty()) {
+			int[] loc = q.poll();
+			for(int i = 0; i < 4; i++) {
+				int xi = dx[i] + loc[0];
+				int yi = dy[i] + loc[1];
+				int cnt = loc[2];
+				
+				if(xi >= 0 && yi >= 0 && xi < n && yi < m) {
+					if(!visited[xi][yi] && map[xi][yi] == 1) {
+						visited[xi][yi] = true;
+						result[xi][yi] = cnt + 1;
+						q.offer(new int[]{xi, yi, result[xi][yi]});
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		map = new int[n][m];
+		result = new int[n][m];
+		visited = new boolean[n][m];
+		int tx = 0, ty = 0;
+
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 0) result[i][j] = 0;
+				else result[i][j] = -1;
+			}
+		}
+
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(map[i][j] == 2) {
+					tx = i; ty = j;
+					break;
+				}
+			}
+		}
+
+		result[tx][ty] = 0;
+		bfs(tx, ty);
+
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				System.out.print(result[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
