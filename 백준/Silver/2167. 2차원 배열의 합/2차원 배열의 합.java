045017eae0e5700759f626d16d4f4d576c334d41
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-10 15:16:55 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 444 ms, Memory: 28380 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -10,13 +10,20 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 
 		int[][] arr = new int[N + 1][M + 1];
+		int[][] prefixSum = new int[N + 1][M + 1];
 
-		for(int i = 1; i <= N; i++) {
+		for(int i = 1; i <= N; i++) { 
 			st = new StringTokenizer(br.readLine());
 			for(int j = 1; j <= M; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+
+		for(int i = 1; i <= N; i++) {
+    		for(int j = 1; j <= M; j++) {
+    			prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + arr[i][j]; 
+    		}
+    	}
 		
 		int K = Integer.parseInt(br.readLine());
 		for(int t = 0; t < K; t++) {
@@ -27,13 +34,7 @@ public class Main {
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 
-			int sum = 0;
-			for(int n = i; n <= x; n++) {
-				for(int m = j; m <= y; m++) {
-					sum += arr[n][m];
-				}
-			}
-			System.out.println(sum);
+			System.out.println(prefixSum[x][y] - prefixSum[i - 1][y] - prefixSum[x][j - 1] + prefixSum[i - 1][j - 1]);
 		}
 	}
 }
