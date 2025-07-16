81897323aa19d7beb772f235af8f861091e7a1cc
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-16 09:29:04 +0900

    [Gold V] Title: 트리, Time: 120 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,55 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    //인접리스트
-    static ArrayList<ArrayList<Integer>> tree;
-    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int R;
+    static int cnt = 0;
 
-    static void dfs(int data) {
-        visited[data] = true;
+    static void dfs(int start) {
+        if(start == R) return;
 
-        for(int i = 0; i < tree.get(data).size(); i++) {
-            int d = tree.get(data).get(i);
-            if(!visited[d]) {
-                dfs(tree.get(data).get(i));
+        for(int v : graph.get(start)) {
+            if(v != R) { // 자식 노드가 지울 노드가 아닐 때
+                if(graph.get(v).size() == 0) cnt++; // 자식 노드가 리프 노드라면 cnt++;
+                else dfs(v);
+            } else { // 자식 노드가 지울 노드라면
+                if(graph.get(start).size() == 1) cnt++; // 부모 노드의 자식이 지울 노드밖에 없다면
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        tree = new ArrayList<ArrayList<Integer>>();
-        visited = new boolean[n];
-        
-        for(int i = 0; i < n; i++) {
-            tree.add(new ArrayList<Integer>());
-        }
+
+        int N = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            tree.add(new ArrayList<Integer>());
-            int pN = Integer.parseInt(st.nextToken());
-            if(pN == -1) {
+        for(int i = 0; i < N; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+
+        int start = 0;
+        for(int i = 0; i < N; i++) {
+            int v = Integer.parseInt(st.nextToken());
+            if(v == -1) {
+                start = i;
                 continue;
             }
-            tree.get(pN).add(i);
+            else graph.get(v).add(i);  
         }
-        int dN = Integer.parseInt(br.readLine());
-        dfs(dN);
 
-        int count = 0;
-        for(int i = 0; i < n; i++) {
-            tree.get(i).remove(Integer.valueOf(dN));
-            if(!visited[i] && tree.get(i).size() == 0) {
-                count++;
-            }
-        }
-        System.out.println(count);
+        R = Integer.parseInt(br.readLine());
+
+        dfs(start);
+        System.out.println(cnt);
     }
 }
