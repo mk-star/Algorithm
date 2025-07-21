1825582b97226d0755e12e5d65436ad560a9aa45
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-21 16:04:03 +0900

    [Gold V] Title: 회의실 배정, Time: 472 ms, Memory: 43612 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,33 +1,36 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[][] time = new int[n][2];
-        for(int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+       
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            time[i][0] = Integer.parseInt(st.nextToken());
-            time[i][1] = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-          Arrays.sort(time, (o1, o2) -> {
-            if(o1[1] == o2[1]) {
-                return o1[0] - o2[0];
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if(a[1] == b[1]) return a[0] - b[0];
+                else return a[1] - b[1];
             }
-            return o1[1] - o2[1];
         });
 
-        int count = 0;
-        int end = 0;
-        for (int i = 0; i < n; i++) {
-            if(end <= time[i][0]) {
-                end = time[i][1];
-                count++;
+        int room = arr[0][1];
+        int cnt = 1;
+        for(int i = 1; i < N; i++) {
+            if(room <= arr[i][0]) {
+                room = arr[i][1];
+                cnt++;
             }
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
