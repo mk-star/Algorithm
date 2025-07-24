c8637ee37d3f212fa3ae2a5ae90856c8223accc1
MinGyeong Kim <ssunn0812@gmail.com>
2025-07-24 16:49:50 +0900

    [Silver I] Title: 나이트의 이동, Time: 288 ms, Memory: 80396 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[][] arr;
+    static boolean[][] visited;
+    static int l;
+    static int targetX, targetY;
+    static int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2};
+    static int[] dy = {1, -1, 2, -2, 2, -2, 1, -1};
+
+    static int bfs(int i, int j) {
+        if(i == targetX && j == targetY) {
+            return 0;
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {i, j, 1});
+        visited[i][j] = true;
+
+        while(!queue.isEmpty()) {
+            int[] tmp = queue.poll();
+
+            for(int t = 0; t < 8; t++) {
+                int x = tmp[0] + dx[t];
+                int y = tmp[1] + dy[t];
+                int cnt = tmp[2];
+
+                if(x == targetX && y == targetY) {
+                    return cnt;
+                }
+
+                if(x >= 0 && x < l && y >= 0 && y < l) {
+                    if(!visited[x][y]) {
+                        visited[x][y] = true;
+                        queue.offer(new int[] {x, y, cnt + 1});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+            l = Integer.parseInt(br.readLine());
+            arr = new int[l][l];
+            visited = new boolean[l][l];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int nowX = Integer.parseInt(st.nextToken());
+            int nowY = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            targetX = Integer.parseInt(st.nextToken());
+            targetY = Integer.parseInt(st.nextToken());
+
+            System.out.println(bfs(nowX, nowY));
+        }
+    }
+}
+
