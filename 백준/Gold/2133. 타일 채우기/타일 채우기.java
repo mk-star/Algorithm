9c251fe665077744aad40d415c17efc956da734a
MinGyeong Kim <ssunn0812@gmail.com>
2025-08-02 10:35:30 +0900

    [Gold IV] Title: 타일 채우기, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        dp[0] = 1;
+        dp[2] = 3;
+ 
+        for(int i = 4; i <= N; i += 2) {
+            dp[i] = dp[i - 2] * dp[2];
+            for(int j = 0; j < i - 2; j += 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
