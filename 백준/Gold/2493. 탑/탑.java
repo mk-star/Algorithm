4ec649f128c5ed444a6999da13387fd8a711fa26
MinGyeong Kim <ssunn0812@gmail.com>
2025-08-03 11:03:26 +0900

    [Gold V] Title: 탑, Time: 2496 ms, Memory: 140800 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -7,28 +7,26 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        HashMap<Integer, Integer> map = new HashMap<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
             arr[i] = num;
-            map.put(num, i);
         }
 
-        Stack<Integer> stack = new Stack<>();
+        Stack<int[]> stack = new Stack<>();
         ArrayList<Integer> result = new ArrayList<>();
 
         for(int i = 0; i < N; i++) {
             while(!stack.isEmpty()) {
-                if(stack.peek() >= arr[i]) {
+                if(stack.peek()[0] >= arr[i]) {
                     break;
                 }
                 stack.pop();
             }
             if(stack.isEmpty()) result.add(0);
-            else result.add(map.get(stack.peek()) + 1);
-            stack.push(arr[i]);
+            else result.add(stack.peek()[1]);
+            stack.push(new int[] { arr[i], i + 1 });
         }
 
         for(int n : result) {
