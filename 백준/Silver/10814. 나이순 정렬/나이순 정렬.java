42af691a7b3d7f73a2c9f1d9f693824d02950980
MinGyeong Kim <ssunn0812@gmail.com>
2025-08-16 10:25:06 +0900

    [Silver V] Title: 나이순 정렬, Time: 376 ms, Memory: 41604 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,43 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-class User implements Comparable<User> {
-	int age;
-	String name;
-	int order;
-
-	User(int age, String name, int order) {
-		this.age = age;
-		this.name = name;
-		this.order = order;
-	}
-
-	public int compareTo(User u) {
-		if(u.age == this.age) return this.order - u.order;
-		else return this.age - u.age;
-	}
-}
-
 public class Main {
 	public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-		User[] arr = new User[N];
+
+		StringBuilder[] p = new StringBuilder[201];
+		for(int i = 0; i < p.length; i++) {
+			p[i] = new StringBuilder();
+		}
 
 		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
 			int age = Integer.parseInt(st.nextToken());
 			String name = st.nextToken();
 
-			arr[i] = new User(age, name, i);
+			p[age].append(age).append(' ').append(name).append('\n');
 		}
 
-		Arrays.sort(arr);
-
-		for(User u : arr) {
-			System.out.println(u.age + " " + u.name);
+		StringBuilder sb = new StringBuilder();
+		for(StringBuilder val : p) {
+			sb.append(val);
 		}
+
+		System.out.println(sb);
 	}
 }
