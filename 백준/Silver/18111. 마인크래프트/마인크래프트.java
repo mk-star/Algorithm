8f633a022eb9f263a38b0c582ba4c935173509e9
MinGyeong Kim <ssunn0812@gmail.com>
2025-08-18 11:30:42 +0900

    [Silver II] Title: 마인크래프트, Time: 712 ms, Memory: 35752 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		int[][] ground = new int[N][M];
+
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				ground[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int min = Integer.MAX_VALUE;
+		int height = 0;
+		for(int h = 0; h <= 256; h++) {
+			int b = B;
+			int time = 0;
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < M; j++) {
+					if(ground[i][j] > h) {
+						b += ground[i][j] - h;
+						time += 2 * (ground[i][j] - h);
+					} else if(ground[i][j] < h) {
+						b -= h - ground[i][j];
+						time += h - ground[i][j];
+					}
+				}
+			}
+
+			if(b < 0) continue;
+			if(time <= min) {
+				min = time;
+				height = Math.max(height, h);
+			}
+		}
+		System.out.println(min + " " + height);
+	}
+}
