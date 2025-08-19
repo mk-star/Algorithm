087db6ff05d5c4db0569cdd60952e2cdc3d52b5b
MinGyeong Kim <ssunn0812@gmail.com>
2025-08-19 11:17:19 +0900

    [level 2] Title: H-Index, Time: 0.93 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,34 +1,14 @@
 import java.util.*;
-// 1 2 4 5 6
 class Solution {
-    public boolean binarySearch(int[] citations, int h) {
-        int left = 0;
-        int right = citations.length - 1;
-        
-        int idx = 0;
-        while(left <= right) {
-            int mid = (left + right) / 2;
-            
-            if(citations[mid] < h) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-                idx = mid;
-            }
-        }
-
-        if(citations.length - idx >= h) return true;
-        else return false;
-    }
-    
     public int solution(int[] citations) {  
         Arrays.sort(citations);
-        int max = citations[citations.length - 1];
-        
+
         int answer = 0;
-        for(int i = max; i >= 0; i--) {
-            if(binarySearch(citations, i)) {
-                answer = i;
+        int N = citations.length;
+        for(int i = 0; i < N; i++) {
+            int index = N - i;
+            if (citations[i] >= index) {
+                answer = index;
                 break;
             }
         }
