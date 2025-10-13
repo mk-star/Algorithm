32e16f4f1fcfc408ba8329a5a3102d6c5fd13417
MinGyeong Kim <ssunn0812@gmail.com>
2025-10-13 22:42:30 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,26 +1,28 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
 
-        int i = 1;
         int result = 0;
-        while(i < N) {
+        for(int i = 1; i <= N; i++) {
             int sum = i;
-            int temp = i;
+
+            int temp  = i;
             while(temp != 0) {
                 sum += temp % 10;
-                temp /= 10;
+                temp /= 10; 
             }
+
             if(sum == N) {
                 result = i;
                 break;
             }
-            i++;
         }
         System.out.println(result);
-    }
+	}
 }
