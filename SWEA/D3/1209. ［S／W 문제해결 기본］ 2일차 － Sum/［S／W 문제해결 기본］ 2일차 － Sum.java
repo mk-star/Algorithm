2de83048f24e3148ddf0154254994b03ec451f98
MinGyeong Kim <ssunn0812@gmail.com>
2025-11-20 19:22:54 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 127 ms, Memory: 36,292 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -12,49 +12,37 @@ class Solution {
 
             int T = Integer.parseInt(br.readLine());
 
-            int[][] arr = new int[100][100];
+            int N = 100;
+            // 0~99 -> 행의 합
+            // 100~199 -> 열의 합
+            // 200 -> 아래 대각선의 합
+            // 201 -> 위 대각선의 합
+            int[] sum = new int[N * 2 + 2];
 
             for(int i = 0; i < 100; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int sum = 0;
                 for(int j = 0; j < 100; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                    sum += arr[i][j];
+                    int num = Integer.parseInt(st.nextToken());
+            
+                    // 행의 합
+                    sum[i] += num;
+
+                    // 열의 합
+                    sum[j + N] += num;
+
+                    // 아래 대각선의 합
+                    if(i == j) sum[N * 2] += num;
+
+                    // 위 대각선의 합
+                    if(i + j == N - 1) sum[N * 2 + 1] += num;
                 }
             }
 
             int max = Integer.MIN_VALUE;
             // 가로
-            for(int i = 0; i < 100; i++) {
-                int sum = 0;
-                for(int j = 0; j < 100; j++) {
-                    sum += arr[i][j];
-                }
-                max = Math.max(sum, max);
+            for(int s : sum) {
+                max = Math.max(s, max);
             }
-
-            // 세로
-            for(int i = 0; i < 100; i++) {
-                int sum = 0;
-                for(int j = 0; j < 100; j++) {
-                    sum += arr[j][i];
-                }
-                max = Math.max(sum, max);
-            }
-
-            // 아래 대각선
-            int sum = 0;
-            for(int i = 0; i < 100; i++) {
-                sum += arr[i][i];
-            }
-            max = Math.max(sum, max);
-
-            // 위 대각선
-            sum = 0;
-            for(int i = 0; i < 100; i++) {
-                sum += arr[i][100 - i - 1];
-            }
-            max = Math.max(sum, max);
 
             sb.append('#').append(T).append(' ').append(max).append('\n');
 		}
